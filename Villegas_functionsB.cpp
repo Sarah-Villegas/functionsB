765a375331809b6870205bb1ff0c39e5c58168df
Sarah Villegas <sarahbvillegas@student.hartnell.edu>
2015-10-15 15:31:04 -0700

    added the rest of the test

--- Villegas_functionsB.cpp
+++ Villegas_functionsB.cpp
@@ -48,8 +48,32 @@ int main(){
     assert(fabs(computeRectangle( 7.6,9.3 ) - 70.68 ) < EPS);
     assert(fabs(computeRectangle( 11.2, 7.2 ) - 80.64) < EPS);
     
-    //assert(fabs(calcArea(, ) ) < EPS);
-    //assert(fabs(calcArea(, ) ) < EPS);
+    double tested2; 
+    computerArea(8,3, tested2); 
+    cout <<  "answer should be 24" << endl;
+    computerArea(7,6, tested2); 
+    cout <<  "answer should be 42" << endl;
+    
+    double tested3, tested4; 
+    computerArea(5, 9, tested3, tested4);
+    cout <<  "answer should be 45, 28" << endl;
+    computerArea(8, 7, tested3, tested4);
+    cout <<  "answer should be 56, 30" << endl;
+    
+    double tested5, tested6; 
+    stats(8, 2, 8, 5, tested5,  tested6);
+    cout <<  "answer should be 5.75, 23" << endl;
+    stats(7,9, 2, 4, tested5, tested6);
+    cout <<  "answer should be 5.5, 22" << endl;
+    
+    double tested7, tested8; 
+    calcAreaPerimeter(2.7, tested7, tested8);
+    cout <<  "answer should be 16.956, 22.8906" << endl;
+    calcAreaPerimeter(4.3, tested7, tested8);
+    cout <<  "answer should be 27.004, 58.0586" << endl;
+    
+    assert(fabs(calcArea(7.3,8.9 ) - 64.97  ) < EPS);
+    assert(fabs(calcArea(42.9,7.8 ) - 334.62 ) < EPS);
 
     return 0; 
 } 
@@ -70,7 +94,7 @@ void feetToInches(double val1,  double& val2){
 }
 
 double computeRectangle(double valA, double valB){
-    int area;
+    double area;
     area = valA*valB;
     return area;  
 }
@@ -79,27 +103,27 @@ void computerArea(double valA, double valB, double& valC){
    double height = valA;
    double width = valB;
    valC = height * width;
-   
+   cout << valC << endl;
 }
 
 void computerArea(double valA, double valB, double& valArea, double& valPerimeter){
     valArea = valA * valB;
-    valPerimeter = (2*(valA)) + (valB*2);
-   
+    valPerimeter = (2*(valA)) + (valB*2);   
+    cout  << valArea << " "<< valPerimeter << endl; 
 }
 
 
 void stats(double valA, double valB, double valC, double valD, double& valE, double& valF){
     valE = valA + valB + valC + valD/ 4;
     valF = valA + valB + valC + valD;
-   
+   cout << valE << " " << valF << endl;
 }
 
 void calcAreaPerimeter(double radius, double& area, double& perimeter){
     const double PI = 3.14;
     area = (2*PI)*radius;
     perimeter = PI*(radius*radius);
-    
+    cout << area << " " << perimeter << endl; 
 }
 
 
